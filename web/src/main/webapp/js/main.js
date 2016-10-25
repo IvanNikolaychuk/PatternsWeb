5b49540bfc01b5db053c93d3769fcd20b909790c
Иван <NikolaychukIvan@yandex.ru>
2016-10-25 19:26:08 +0300

    Getting rid of html duplicates via angular directices

--- web/src/main/webapp/js/main.js
+++ web/src/main/webapp/js/main.js
@@ -1,32 +1,33 @@
-var app = angular.module("app", [])
-    .service('articleService', function () {
-        return {
-            format: function (date) {
-                return date.dayOfMonth + '.' + date.monthValue + '.' + date.year;
+(function() {
+    var app = angular.module("app", [])
+        .service('articleService', function () {
+            return {
+                format: function (date) {
+                    return date.dayOfMonth + '.' + date.monthValue + '.' + date.year;
+                }
+            };
+        })
+        .controller('mainController', ['$scope', '$http', 'articleService', function ($scope, $http, articleService) {
+            $scope.articleService = articleService;
+
+
+            var desiredTag = getParameterByName('tag');
+            var url = desiredTag == null ? "rest/articles" : "rest/articles?tag=" + desiredTag;
+
+            $http.get(url)
+                .then(function (response) {
+                    $scope.articles = response.data;
+                    $scope.filterTag = desiredTag;
+                });
+
+
+            function getParameterByName(name) {
+                name = name.replace(/[\[\]]/g, "\\$&");
+                var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+                    results = regex.exec(window.location.href);
+                if (!results) return null;
+                if (!results[2]) return '';
+                return decodeURIComponent(results[2].replace(/\+/g, " "));
             }
-        };
-    })
-    .controller('mainController', ['$scope', '$http', 'articleService', function ($scope, $http, articleService) {
-        $scope.articleService = articleService;
-
-
-        var desiredTag = getParameterByName('tag');
-        var url = desiredTag == null ? "rest/articles" : "rest/articles?tag=" + desiredTag;
-
-        $http.get(url)
-            .then(function (response) {
-                $scope.articles = response.data;
-                $scope.filterTag = desiredTag;
-            });
-
-
-        function getParameterByName(name) {
-            name = name.replace(/[\[\]]/g, "\\$&");
-            var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-                results = regex.exec(window.location.href);
-            if (!results) return null;
-            if (!results[2]) return '';
-            return decodeURIComponent(results[2].replace(/\+/g, " "));
-        }
-    }]);
-
+        }])
+})();
